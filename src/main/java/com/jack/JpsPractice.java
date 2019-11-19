a90ea2527fd17dfe4bd67ad3785814a6763a5d0f
zhaoma3 <zhaoma3@creditease.cn>
2019-11-19 15:14:58 +0800

    jps实践

---
+++ src/main/java/com/jack/JpsPractice.java
@@ -0,0 +1,13 @@
+package com.jack;
+
+/**
+ * 查看java进程命令
+ * (ps -ef|grep java)  &  (ls -l)  ->  jps -l
+ *
+ *
+ * */
+public class JpsPractice {
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(Integer.MAX_VALUE);
+    }
+}
