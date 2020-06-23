a5e04b47457754046b4bf652e629267ca995e1b5
zhaoma3 <zhaoma3@creditease.cn>
2020-06-23 14:55:22 +0800

    commit spring-actuator

--- src/main/java/com/jack/PracticeApplication.java
+++ src/main/java/com/jack/PracticeApplication.java
@@ -1,15 +1,26 @@
 package com.jack;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@RestController
+@Slf4j
 public class PracticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeApplication.class, args);
 	}
 
+	@RequestMapping("/test")
+	public String test() {
+		log.debug("debug测试");
+		log.info("查看输出位置");
+		return "success";
+	}
 }
